70e8492d0ecdb91cf02d2ec8c19bf77a824d3a9c
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-08-06 20:25:32 +0900

    a

---
+++ flowToday/app/src/main/java/com/bestswlkh0310/data/Tables.kt
@@ -0,0 +1,7 @@
+package com.bestswlkh0310.data
+object Tables {
+
+    const val DATABASE = "flay_database"
+
+    const val STOP_WATCH = "stop_watch"
+}
---
+++ flowToday/app/src/main/java/com/bestswlkh0310/data/entity/StopWatchEntity.kt
@@ -0,0 +1,8 @@
+package com.bestswlkh0310.data.entity
+
+import java.time.LocalDateTime
+
+data class StopWatchEntity(
+    val title: String,
+    val deadline: LocalDateTime
+)
